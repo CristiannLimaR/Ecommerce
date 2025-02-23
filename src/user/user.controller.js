ff0447b62ba85fe1e2c5c1644d79300aadea02ae
Cristian Lima <cristiannlima2@gmail.com>
2025-02-22 19:06:02 -0600

    functions of admin validator

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -1,5 +1,5 @@
 import { response, request } from "express";
-import { hash} from "argon2";
+import { hash, verify} from "argon2";
 import User from "./user.model.js";
 
 
@@ -54,28 +54,55 @@ export const getUserById = async (req = request, res) => {
   }
 };
 
+
 export const updateUser = async (req, res = response) => {
   try {
     const { id } = req.params;
-    const { password, ...data } = req.body;
-    const authenticatedUser = req.user;
+    const { currentPassword, password, role, ...data } = req.body;
 
-    if (authenticatedUser.id !== id) {
+    if (req.user.role !== "ADMIN" && req.user.id !== id) {
       return res.status(403).json({
         success: false,
         msg: "You can only update your own account",
       });
     }
 
+    if (req.user.role !== "ADMIN" && role) {
+      return res.status(403).json({
+        success: false,
+        msg: "You are not allowed to change your role",
+      });
+    }
+
+    
     if (password) {
+      if (!currentPassword) {
+        return res.status(400).json({
+          success: false,
+          msg: "Current password is required to update the password",
+        });
+      }
+
+      const user = await User.findById(id);
+      const validPassword = await verify( user.password, currentPassword);
+
+      if (!validPassword) {
+        return res.status(400).json({
+          success: false,
+          msg: "Current password is incorrect",
+        });
+      }
+
+      
       data.password = await hash(password);
     }
-    const user = await User.findByIdAndUpdate(id, data, { new: true });
+
+    const updatedUser = await User.findByIdAndUpdate(id, data, { new: true });
 
     res.status(200).json({
       success: true,
       msg: "User successfully updated",
-      user,
+      user: updatedUser,
     });
   } catch (error) {
     res.status(500).json({
@@ -86,12 +113,12 @@ export const updateUser = async (req, res = response) => {
   }
 };
 
+
 export const deleteUser = async (req, res) => {
   try {
     const { id } = req.params;
-    const authenticatedUser = req.user;
 
-    if (authenticatedUser.id !== id) {
+    if (req.user.role !== "ADMIN" && req.user.id !== id) {
       return res.status(403).json({
         success: false,
         msg: "You can only delete your own account",
@@ -112,7 +139,6 @@ export const deleteUser = async (req, res) => {
     res.status(200).json({
       success: true,
       msg: "User deactivated successfully",
-      user,
     });
   } catch (error) {
     res.status(500).json({
